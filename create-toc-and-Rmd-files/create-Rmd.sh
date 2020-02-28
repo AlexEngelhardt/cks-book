6cb07a893fef404f6c04f0faa7efa11cf31ac6f8
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2020-02-28 12:29:16 +0100

    empty Rmds added

---
+++ create-toc-and-Rmd-files/create-Rmd.sh
@@ -0,0 +1,187 @@
+echo "## Grundlagen" > "rmd/0-1--grundlagen.Rmd"
+echo "### Grundgesamtheit, Stichprobe, Merkmale" > "rmd/0-1-1--grundgesamtheit-stichprobe-merkmale.Rmd"
+echo "### Merkmals- und Skalentypen
+#### Merkmalstypen: Diskrete und stetige Merkmale
+#### Skalentypen" > "rmd/0-1-2--merkmals-und-skalentypen.Rmd"
+echo "### Objektivität, Reliabilität, Validität" > "rmd/0-1-3--objektivitaet-reliabilitaet-validitaet.Rmd"
+echo "### Mathematische Grundlagen" > "rmd/0-1-4--mathematische-grundlagen.Rmd"
+#### Prozentrechnung
+echo "#### Mathematische Symbole" > "rmd/0-1-4-2--mathematische-symbole.Rmd"
+echo "#### Das griechische Alphabet" > "rmd/0-1-4-3--das-griechische-alphabet.Rmd"
+echo "#### Das Summenzeichen Σ und Rechenregeln" > "rmd/0-1-4-4--das-summenzeichen-und-rechenregeln.Rmd"
+echo "#### Das Produktzeichen Π und Rechenregeln" > "rmd/0-1-4-5--das-produktzeichen-und-rechenregeln.Rmd"
+echo "#### Exponentialfunktion und Logarithmusfunktion" > "rmd/0-1-4-6--exponentialfunktion-und-logarithmusfunktion.Rmd"
+echo "#### Fakultät N! und Binomialkoeffizient (Nk)" > "rmd/0-1-4-7--fakultaet-und-binomialkoeffizient.Rmd"
+#### Lineartransformationen
+#### Ableiten
+#### Integrieren
+
+
+echo "# I. Daten beschreiben: Deskriptive Statistik" > "rmd/1--deskriptive-statistik.Rmd"
+
+echo "## Eine Variable" > "rmd/1-1--eine-variable.Rmd"
+echo "### Häufigkeitstabellen" > "rmd/1-1-1--haeufigkeitstabellen.Rmd"
+echo "### Lageparameter
+#### Arithmetischer Mittelwert
+#### Median
+#### Modus" > "rmd/1-1-2--lageparameter.Rmd"
+echo "#### Quantile" > "rmd/1-1-2-4--quantile.Rmd"
+echo "### Streuungsparameter" > "rmd/1-1-3--streuungsparameter.Rmd"
+#### Spannweite und Quartilsabstand
+#### Varianz und Standardabweichung
+echo "#### Variationskoeffizient" > "rmd/1-1-3-3--variationskoeffizient.Rmd"
+echo "### Konzentrationsparameter" > "rmd/1-1-4--konzentrationsparameter.Rmd"
+echo "#### Lorenzkurve" > "rmd/1-1-4-1--lorenzkurve.Rmd"
+echo "#### Gini-Koeffizient" > "rmd/1-1-4-2--gini-koeffizient.Rmd"
+echo "### Darstellung von Daten" > "rmd/1-1-5--darstellung-von-daten.Rmd"
+echo "#### Balkendiagramme" > "rmd/1-1-5-1--balkendiagramme.Rmd"
+echo "#### Verteilungsfunktion" > "rmd/1-1-5-2--verteilungsfunktion.Rmd"
+echo "#### Boxplots" > "rmd/1-1-5-3--boxplots.Rmd"
+echo "#### Histogramme" > "rmd/1-1-5-4--histogramme.Rmd"
+
+echo "## Zwei und mehr Variablen" > "rmd/1-2--zwei-und-mehr-variablen.Rmd"
+echo "### Zwei diskrete Variablen" > "rmd/1-2-1--zwei-diskrete-variablen.Rmd"
+echo "#### Kreuztabellen / Kontingenztafeln
+##### Bedingte Häufigkeiten" > "rmd/1-2-1-1--kreuztabellen-kontingenztafeln.Rmd"
+echo "#### Zusammenhangsmaße" > "rmd/1-2-1-2--zusammenhangsmasse.Rmd"
+echo "##### \(\chi^2\)-Koeffizient und Kontingenzkoeffizient K" > "rmd/1-2-1-2-1--chi-quadrat-koeffizient-und-kontingenzkoeffizient-k.Rmd"
+echo "##### ϕ-Koeffizient" > "rmd/1-2-1-2-2--phi-koeffizient.Rmd"
+echo "### Zwei stetige Variablen" > "rmd/1-2-2--zwei-stetige-variablen.Rmd"
+echo "#### Streudiagramme" > "rmd/1-2-2-1--streudiagramme.Rmd"
+echo "#### Zusammenhangsmaße" > "rmd/1-2-2-2--zusammenhangsmasse.Rmd"
+echo "##### Korrelation und Kausalität" > "rmd/1-2-2-2-1--korrelation-und-kausalitaet.Rmd"
+echo "##### Korrelation r nach Pearson
+###### Empirische Kovarianz" > "rmd/1-2-2-2-2--korrelation-r-nach-pearson.Rmd"
+echo "##### Spearman-Korrelation / Rangkorrelation" > "rmd/1-2-2-2-3--spearman-korrelation-rangkorrelation.Rmd"
+
+
+echo "# II. Wahrscheinlichkeitsrechnung" > "rmd/2--wahrscheinlichkeitsrechnung.Rmd"
+
+echo "## Kombinatorik" > "rmd/2-1--kombinatorik.Rmd"
+echo "### Kombinatorik: Ein Überblick" > "rmd/2-1-1--kombinatorik-ein-ueberblick.Rmd"
+echo "### Kombinationen - ohne Reihenfolge
+#### Ziehen ohne Zurücklegen
+#### Ziehen mit Zurücklegen" > "rmd/2-1-2--kombinationen-ohne-reihenfolge.Rmd"
+echo "### Variationen - mit Reihenfolge
+#### Ziehen ohne Zurücklegen
+#### Ziehen mit Zurücklegen" > "rmd/2-1-3--variationen-mit-reihenfolge.Rmd"
+echo "### Permutationen
+#### Falls Elemente in der Ausgangsmenge gleich sind" > "rmd/2-1-4--permutationen.Rmd"
+
+echo "## Wahrscheinlichkeiten" > "rmd/2-2--wahrscheinlichkeiten.Rmd"
+echo "### Was ist eine Wahrscheinlichkeit?
+#### Laplace-Wahrscheinlichkeiten" > "rmd/2-2-1--was-ist-eine-wahrscheinlichkeit.Rmd"
+echo "### Mengenlehre und Venn-Diagramme" > "rmd/2-2-2--mengenlehre-und-venn-diagramme.Rmd"
+echo "### Bedingte Wahrscheinlichkeiten" > "rmd/2-2-3--bedingte-wahrscheinlichkeiten.Rmd"
+echo "### Satz der totalen Wahrscheinlichkeit" > "rmd/2-2-4--satz-der-totalen-wahrscheinlichkeit.Rmd"
+echo "### Unabhängigkeit von Ereignissen" > "rmd/2-2-5--unabhaengigkeit-von-ereignissen.Rmd"
+echo "### Satz von Bayes" > "rmd/2-2-6--satz-von-bayes.Rmd"
+echo "### Anleitung zum Rechnen mit Wahrscheinlichkeiten" > "rmd/2-2-7--anleitung-zum-rechnen-mit-wahrscheinlichkeiten.Rmd"
+
+echo "## Zufallsvariablen" > "rmd/2-3--zufallsvariablen.Rmd"
+echo "### Was sind Zufallsvariablen?
+#### Diskrete und stetige Zufallsvariablen" > "rmd/2-3-1--was-sind-zufallsvariablen.Rmd"
+echo "### Unabhängigkeit zweier Zufallsvariablen" > "rmd/2-3-2--unabhaengigkeit-zweier-zufallsvariablen.Rmd"
+echo "### Darstellung und Eigenschaften von diskreten Zufallsvariablen
+#### Dichte
+#### Verteilungsfunktion
+#### Quantile
+#### Erwartungswert
+#### Varianz und Standardabweichung" > "rmd/2-3-3--darstellung-und-eigenschaften-von-diskreten-zufallsvariablen.Rmd"
+echo "### Darstellung und Eigenschaften von stetigen Zufallsvariablen
+#### Dichte
+#### Verteilungsfunktion
+#### Quantilsfunktion
+#### Umwandeln von der Dichte zur Verteilungsfunktion/Quantilsfunktion und wieder zurück
+#### Erwartungswert
+#### Varianz und Standardabweichung" > "rmd/2-3-4--darstellung-und-eigenschaften-von-stetigen-zufallsvariablen.Rmd"
+echo "### Rechenregeln für Erwartungswert und Varianz
+#### Erwartungswert
+#### Varianz" > "rmd/2-3-5--rechenregeln-fuer-erwartungswert-und-varianz.Rmd"
+
+echo "## Verteilungen" > "rmd/2-4--verteilungen.Rmd"
+echo "### Diskrete Verteilungen" > "rmd/2-4-1--diskrete-verteilungen.Rmd"
+echo "#### Diskrete Gleichverteilung: Ein Würfelwurf" > "rmd/2-4-1-1--diskrete-gleichverteilung.Rmd"
+echo "#### Bernoulliverteilung: Ein Münzwurf" > "rmd/2-4-1-2--bernoulliverteilung.Rmd"
+echo "#### Binomialverteilung: Zehn Münzwürfe" > "rmd/2-4-1-3--binomialverteilung.Rmd"
+echo "#### Hypergeometrische Verteilung" > "rmd/2-4-1-4--hypergeometrische-verteilung.Rmd"
+echo "#### Geometrische Verteilung: Der betrunkene Pförtner" > "rmd/2-4-1-5--geometrische-verteilung.Rmd"
+echo "#### Poissonverteilung: Anzahl an Toren pro Fußballspiel" > "rmd/2-4-1-6--poissonverteilung.Rmd"
+echo "### Stetige Verteilungen" > "rmd/2-4-2--stetige-verteilungen.Rmd"
+echo "#### Stetige Gleichverteilung: Wartezeit auf den Bus" > "rmd/2-4-2-1--stetige-gleichverteilung.Rmd"
+echo "#### Normalverteilung: Körpergrösse" > "rmd/2-4-2-2--normalverteilung.Rmd"
+echo "#### Exponentialverteilung: Wartezeit bis Glühbirnenausfall" > "rmd/2-4-2-3--exponentialverteilung.Rmd"
+echo "#### t-Verteilung: Stichprobenmittelwerte" > "rmd/2-4-2-4--t-verteilung.Rmd"
+echo "#### χ2-Verteilung" > "rmd/2-4-2-5--chi-quadrat-verteilung.Rmd"
+echo "### Verteilungstabellen und Anleitung zum Ablesen" > "rmd/2-4-3--verteilungstabellen-und-anleitung-zum-ablesen.Rmd"
+echo "#### Tabelle Normalverteilung" > "rmd/2-4-3-1--tabelle-normalverteilung.Rmd"
+echo "#### Tabelle t-Verteilung" > "rmd/2-4-3-2--tabelle-t-verteilung.Rmd"
+echo "#### Tabelle χ2-Verteilung" > "rmd/2-4-3-3--tabelle-chi-quadrat-verteilung.Rmd"
+
+
+echo "# III. Auf die Grundgesamtheit schließen: Inferenzstatistik" > "rmd/3--inferenzstatistik.Rmd"
+echo "### Inferenzstatistik: Eine Übersicht" > "rmd/3-0-1--inferenzstatistik-eine-uebersicht.Rmd"
+
+echo "## Parameterschätzung und Konfidenzintervalle" > "rmd/3-1--parameterschaetzung-und-konfidenzintervalle.Rmd"
+echo "### Was ist ein Parameter?
+#### Wozu brauchen wir Parameterschätzung?
+#### Parameterschätzer sind die Grundlage für Hypothesentests
+#### Was kann man schätzen?
+##### Prozentuale Anteile
+##### Erwartungswert
+##### Varianz
+##### Andere Verteilungsparameter" > "rmd/3-1-1--was-ist-ein-parameter.Rmd"
+echo "### Schätzfunktionen allgemein
+#### Wie schätzt man einen Parameter?" > "rmd/3-1-2--schaetzfunktionen-allgemein.Rmd"
+echo "### Die wichtigsten Parameterschätzer
+#### Prozentualer Anteil
+#### Erwartungswert
+#### Varianz" > "rmd/3-1-3--die-wichtigsten-parameterschaetzer.Rmd"
+echo "### Was ist ein Konfidenzintervall?
+#### Wozu braucht man Konfidenzintervalle?
+#### Was ist ein Konfidenzintervall?
+#### Konfidenzintervalle werden auch nur geschätzt
+#### Wie schätzt man ein Konfidenzintervall?" > "rmd/3-1-4--was-ist-ein-konfidenzintervall.Rmd"
+echo "### Die wichtigsten Konfidenzintervalle" > "rmd/3-1-5--die-wichtigsten-konfidenzintervalle.Rmd"
+echo "#### KI für einen Anteil p" > "rmd/3-1-5-1--ki-fuer-einen-anteil-p.Rmd"
+echo "#### KI für den Erwartungswert μ
+##### Falls σ2--bekannt
+##### Falls σ2--unbekannt" > "rmd/3-1-5-2--ki-fuer-den-erwartungswert-mu.Rmd"
+echo "#### KI für die Varianz σ2" > "rmd/3-1-5-3--ki-fuer-die-varianz-sigma2.Rmd"
+
+echo "## Hypothesentests" > "rmd/3-2--hypothesentests.Rmd"
+echo "### Was sind Hypothesentests?" > "rmd/3-2-1--was-sind-hypothesentests.Rmd"
+echo "### Vorgehen bei Hypothesentests
+#### 1. Hypothesen aufstellen
+##### Einseitige und zweiseitige Tests
+#### 2. Test wählen
+#### 3. Signifikanzniveau festlegen
+##### α- und β-Fehler
+#### 4. Daten sammeln
+#### 5. Prüfgröße berechnen
+#### 6. Verteilung der Prüfgröße bestimmen
+#### 7. Test abschließen: Zwei Möglichkeiten
+##### Von Hand: Über die kritische Schranke und Verteilungstabelle
+##### Per Computer: mit dem p-Wert
+#### 8. Testentscheidung treffen" > "rmd/3-2-2--vorgehen-bei-hypothesentests.Rmd"
+echo "### Was kommt in H0--und was in H1?" > "rmd/3-2-3--was-kommt-in-h0--und-was-in-h1.Rmd"
+echo "### Welchen statistischen Test soll ich wählen?" > "rmd/3-2-4--welchen-statistischen-test-soll-ich-waehlen.Rmd"
+echo "### Einige klausurrelevante Tests" > "rmd/3-2-5--einige-klausurrelevante-tests.Rmd"
+echo "#### Binomialtest: Anteile testen" > "rmd/3-2-5-1--binomialtest.Rmd"
+echo "#### Einstichproben-t-Test: Einzelnen Mittelwert testen" > "rmd/3-2-5-2--einstichproben-t-test.Rmd"
+echo "#### Gepaarter t-Test: Vorher/Nachher-Mittelwertsvergleich" > "rmd/3-2-5-3--gepaarter-t-test.Rmd"
+echo "#### Zweistichproben-t-Test: Mittelwerte zweier Gruppen vergleichen" > "rmd/3-2-5-4--zweistichproben-t-test.Rmd"
+echo "#### Chi-Quadrat-Test: Abhängigkeit zwischen zwei nominalen Variablen testen" > "rmd/3-2-5-5--chi-quadrat-test.Rmd"
+
+echo "## Regression" > "rmd/3-3--regression.Rmd"
+echo "### Ein einführendes Beispiel" > "rmd/3-3-1--ein-einfuehrendes-beispiel.Rmd"
+echo "### Einfache lineare Regression
+#### Berechnen der Parameter a und b
+#### Prognose mit neuen Daten" > "rmd/3-3-2--einfache-lineare-regression.Rmd"
+echo "### Modellannahmen der linearen Regression" > "rmd/3-3-3--modellannahmen-der-linearen-regression.Rmd"
+echo "### Was sind Residuen?" > "rmd/3-3-4--was-sind-residuen.Rmd"
+echo "### Multiple lineare Regression" > "rmd/3-3-5--multiple-lineare-regression.Rmd"
+echo "### Kategorien als Einflussgrößen" > "rmd/3-3-6--kategorien-als-einflussgroesse.Rmd"
+echo "### Konfidenzintervalle für die geschätzten Parameter" > "rmd/3-3-7--konfidenzintervalle-fuer-die-geschaetzten-parameter.Rmd"
+echo "### Testen von Regressionsparametern" > "rmd/3-3-8--testen-von-regressionsparametern.Rmd"
+echo "### Modelldiagnose: Ist mein Modell überhaupt sinnvoll?" > "rmd/3-3-9--modelldiagnose-ist-mein-modell-ueberhaupt-sinnvoll.Rmd"
---
+++ create-toc-and-Rmd-files/entire-toc.md
@@ -0,0 +1,193 @@
+## Grundlagen
+
+### Grundgesamtheit, Stichprobe, Merkmale
+### Merkmals- und Skalentypen
+        Merkmalstypen: Diskrete und stetige Merkmale
+        Skalentypen
+### Objektivität, Reliabilität, Validität
+### Mathematische Grundlagen
+        Prozentrechnung
+        Mathematische Symbole
+        Das griechische Alphabet
+        Das Summenzeichen Σ und Rechenregeln
+        Das Produktzeichen Π und Rechenregeln
+        Exponentialfunktion und Logarithmusfunktion
+        Fakultät N! und Binomialkoeffizient (Nk)
+        Lineartransformationen
+        Ableiten
+        Integrieren
+
+# I. Daten beschreiben: Deskriptive Statistik
+## Eine Variable
+
+### Häufigkeitstabellen
+### Lageparameter
+        Arithmetischer Mittelwert
+        Median
+        Modus
+        Quantile
+### Streuungsparameter
+        Spannweite und Quartilsabstand
+        Varianz und Standardabweichung
+        Variationskoeffizient
+### Konzentrationsparameter
+        Lorenzkurve
+        Gini-Koeffizient
+### Darstellung von Daten
+        Balkendiagramme
+        Verteilungsfunktion
+        Boxplots
+        Histogramme
+
+## Zwei und mehr Variablen
+
+### Zwei diskrete Variablen
+        Kreuztabellen / Kontingenztafeln
+            Bedingte Häufigkeiten
+        Zusammenhangsmaße
+            χ2-Koeffizient und Kontingenzkoeffizient K
+            ϕ-Koeffizient
+### Zwei stetige Variablen
+        Streudiagramme
+        Zusammenhangsmaße
+            Korrelation und Kausalität
+            Korrelation r nach Pearson
+                Empirische Kovarianz
+            Spearman-Korrelation / Rangkorrelation
+
+# II. Wahrscheinlichkeitsrechnung
+## Kombinatorik
+
+### Kombinatorik: Ein Überblick
+### Kombinationen – ohne Reihenfolge
+        Ziehen ohne Zurücklegen
+        Ziehen mit Zurücklegen
+### Variationen – mit Reihenfolge
+        Ziehen ohne Zurücklegen
+        Ziehen mit Zurücklegen
+### Permutationen
+        Falls Elemente in der Ausgangsmenge gleich sind
+
+## Wahrscheinlichkeiten
+
+### Was ist eine Wahrscheinlichkeit?
+        Laplace-Wahrscheinlichkeiten
+### Mengenlehre und Venn-Diagramme
+### Bedingte Wahrscheinlichkeiten
+### Satz der totalen Wahrscheinlichkeit
+### Unabhängigkeit von Ereignissen
+### Satz von Bayes
+### Anleitung zum Rechnen mit Wahrscheinlichkeiten
+
+## Zufallsvariablen
+
+### Was sind Zufallsvariablen?
+        Diskrete und stetige Zufallsvariablen
+### Unabhängigkeit zweier Zufallsvariablen
+### Darstellung und Eigenschaften von diskreten Zufallsvariablen
+        Dichte
+        Verteilungsfunktion
+        Quantile
+        Erwartungswert
+        Varianz und Standardabweichung
+### Darstellung und Eigenschaften von stetigen Zufallsvariablen
+        Dichte
+        Verteilungsfunktion
+        Quantilsfunktion
+        Umwandeln von der Dichte zur Verteilungsfunktion/Quantilsfunktion und wieder zurück
+        Erwartungswert
+        Varianz und Standardabweichung
+### Rechenregeln für Erwartungswert und Varianz
+        Erwartungswert
+        Varianz
+
+## Verteilungen
+
+### Diskrete Verteilungen
+        Diskrete Gleichverteilung: Ein Würfelwurf
+        Bernoulliverteilung: Ein Münzwurf
+        Binomialverteilung: Zehn Münzwürfe
+        Hypergeometrische Verteilung
+        Geometrische Verteilung: Der betrunkene Pförtner
+        Poissonverteilung: Anzahl an Toren pro Fußballspiel
+### Stetige Verteilungen
+        Stetige Gleichverteilung: Wartezeit auf den Bus
+        Normalverteilung: Körpergrösse
+        Exponentialverteilung: Wartezeit bis Glühbirnenausfall
+        t-Verteilung: Stichprobenmittelwerte
+        χ2-Verteilung
+### Verteilungstabellen und Anleitung zum Ablesen
+        Tabelle Normalverteilung
+        Tabelle t-Verteilung
+        Tabelle χ2-Verteilung
+
+# III. Auf die Grundgesamtheit schließen: Inferenzstatistik
+
+    Inferenzstatistik: Eine Übersicht
+
+## Parameterschätzung und Konfidenzintervalle
+
+### Was ist ein Parameter?
+        Wozu brauchen wir Parameterschätzung?
+        Parameterschätzer sind die Grundlage für Hypothesentests
+        Was kann man schätzen?
+            Prozentuale Anteile
+            Erwartungswert
+            Varianz
+            Andere Verteilungsparameter
+### Schätzfunktionen allgemein
+        Wie schätzt man einen Parameter?
+### Die wichtigsten Parameterschätzer
+        Prozentualer Anteil
+        Erwartungswert
+        Varianz
+### Was ist ein Konfidenzintervall?
+        Wozu braucht man Konfidenzintervalle?
+        Was ist ein Konfidenzintervall?
+        Konfidenzintervalle werden auch nur geschätzt
+        Wie schätzt man ein Konfidenzintervall?
+### Die wichtigsten Konfidenzintervalle
+        KI für einen Anteil p
+        KI für den Erwartungswert μ
+            Falls σ2 bekannt
+            Falls σ2 unbekannt
+        KI für die Varianz σ2
+
+## Hypothesentests
+
+### Was sind Hypothesentests?
+### Vorgehen bei Hypothesentests
+        1. Hypothesen aufstellen
+            Einseitige und zweiseitige Tests
+        2. Test wählen
+        3. Signifikanzniveau festlegen
+            α- und β-Fehler
+        4. Daten sammeln
+        5. Prüfgröße berechnen
+        6. Verteilung der Prüfgröße bestimmen
+        7. Test abschließen: Zwei Möglichkeiten
+            Von Hand: Über die kritische Schranke und Verteilungstabelle
+            Per Computer: mit dem p-Wert
+        8. Testentscheidung treffen
+### Was kommt in H0 und was in H1?
+### Welchen statistischen Test soll ich wählen?
+### Einige klausurrelevante Tests
+        Binomialtest: Anteile testen
+        Einstichproben-t-Test: Einzelnen Mittelwert testen
+        Gepaarter t-Test: Vorher/Nachher-Mittelwertsvergleich
+        Zweistichproben-t-Test: Mittelwerte zweier Gruppen vergleichen
+        Chi-Quadrat-Test: Abhängigkeit zwischen zwei nominalen Variablen testen
+
+## Regression
+
+### Ein einführendes Beispiel
+### Einfache lineare Regression
+        Berechnen der Parameter a und b
+        Prognose mit neuen Daten
+### Modellannahmen der linearen Regression
+### Was sind Residuen?
+### Multiple lineare Regression
+### Kategorien als Einflussgrößen
+### Konfidenzintervalle für die geschätzten Parameter
+### Testen von Regressionsparametern
+### Modelldiagnose: Ist mein Modell überhaupt sinnvoll?
